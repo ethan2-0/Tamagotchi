9203ead17c9abc6c34ff3b3e2ec790775e1f692a
ethan2-0 <me@example.com>
2014-03-25 16:49:08 -0400

    More work

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function EbyID(name) {
 function error(message) {
 	EbyID("error-message").innerHTML = message;
 }
+//Note: progressbar is the INNER progressbar, NOT the outer.
+//progressbar should have already been EbyID'd.
+function updateProgressBar(progressbar, value) {
+	progressbar.style.width = "" + (value * 2);
+}
