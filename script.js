8d4baae943cd9fe47fb283d6a8c7b73a6790c508
ethan2-0 <me@example.com>
2014-03-27 17:24:57 -0400

    Finished-ish gui, 300 lines

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ function error(message) {
 }
 //Note: progressbar is the INNER progressbar, NOT the outer.
 //progressbar should have already been EbyID'd.
-function updateProgressBar(progressbar, value) {
-	progressbar.setAttribute("style", "width: " + (value * 2) + "px");
+function updateProgressBar(progressbar, value, color) {
+    if(color == null) {
+        healthRed = 127 - Math.floor(127 * (value / 100));
+        healthGreen = Math.floor((196 * (value / 100)));
+        color = "rgb(" + healthRed + ", " + healthGreen + ", 0)";
+    }
+	progressbar.setAttribute("style", "width: " + (value * 2) + "px; background-color: " + color + ";");
 }
 function getPageName() {
 	return App.getStack()[App.getStack().length - 1][0];
