8199d561e4686fd50ceb96d570d1f29361a192cb
ethan2-0 <me@example.com>
2014-03-28 21:26:59 -0400

    Jumping, Dancing, Dying, Fixing

--- petengine.js
+++ petengine.js
@@ -1,1 +1,133 @@
-alert("hobey-ho")
+/*
+    Petengine.js: Handles pet updating and animation.
+*/
+function changeThePet() {
+    if(getItem("petHealth") > 0) {
+        addItem("petAge", 1);
+        age = getItem("petAge");
+        if(age % 50 == 0) {
+            /*weight = getItem("weight");
+            if(Math.abs(weight - 50) >= 10) {
+                alert("weight");
+                addItem("petHealth", -1);
+            }
+            if(Math.abs(weight - 50) >= 40) {
+                addItem("petHealth", -1);
+            }
+            if(Math.abs(weight - 50) >= 25) {
+                addItem("petHealth", -1);
+            }*/
+            hunger = getItem("petHunger");
+            if(hunger < 40) {
+                alert("hunger");
+                addItem("petHealth", -1);
+            }
+            if(hunger < 25) {
+                addItem("petHealth", -1);
+            }
+            if(hunger < 15) {
+                addItem("petHealth", -1);
+            }
+            if(hunger < 8) {
+                addItem("petHealth", -1);
+            }
+            happiness = getItem("petHappiness");
+            if(happiness < 40) {
+                alert("happiness");
+                addItem("petHealth", -1);
+            }
+            if(happiness < 20) {
+                addItem("petHealth", -1);
+            }
+            if(happiness < 10) {
+                addItem("petHealth", -1);
+            }
+            if(happiness < 5) {
+                addItem("petHealth", -1);
+            }
+            health = getItem("petHealth");
+            if(health < 40) {
+                alert("health");
+                addItem("petHealth", -1);
+            }
+            if(health < 15) {
+                addItem("petHealth", -1);
+            }
+            if(health < 8) {
+                addItem("petHealth", -1);
+            }
+        }
+        if(age % 3000 == 0) {
+            addItem("petHappiness", -1);
+            addItem("petHunger", -1);
+        }
+    }
+}
+danceImage = null;
+x = 0;
+y = 0;
+direction = "left";
+directionTime = 5;
+speed = 2;
+function dance(image) {
+    danceImage = image;
+    for(i = 0; i < 240; i++) {
+        setTimeout(danceOnce, i * 20);
+    }
+    setTimeout(function() {
+        danceImage.setAttribute("style", "");
+        x = 0;
+        y = 0;
+    }, 241 * 20);
+}
+function danceOnce() {
+    directionTime--;
+    if(directionTime <= 0) {
+        r = Math.floor(Math.random() * 5);
+        if(r == 1) {
+            direction = "left";
+        } else if(r == 2) {
+            direction = "right";
+        } else if(r == 3) {
+            direction = "up";
+        } else {
+            direction = "down";
+        }
+        directionTime = Math.floor(Math.random() * 20);
+        speed = Math.floor(directionTime / 4);
+    }
+    if(directionTime % 5 == 0) {
+        speed--;
+    }
+    if(direction == "left") {
+        x -= speed;
+    } else if(direction == "right") {
+        x += speed;
+    } else if(direction == "up") {
+        y -= speed;
+    } else if(direction == "down") {
+        y += speed;
+    }
+    s = "padding-";
+    if(x <= 0) {
+        s += "right";
+    } else {
+        s += "left";
+    }
+    s += ": " + Math.abs(x) + "px; padding-";
+    if(y <= 0) {
+        s += "bottom";
+    } else {
+        s += "top";
+    }
+    s += ": " + Math.abs(y) + "px;";
+    danceImage.setAttribute("style", s);
+}
+//Add the specified amount to an item in localStorage.
+function addItem(name, amount) {
+    localStorage.setItem(name, (localStorage.getItem(name) / 1) + amount)
+}
+//Add the specified amount to an item in localStorage.
+function getItem(name) {
+    return localStorage.getItem(name);
+}
--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 function EbyID(name) {
-	return document.getElementById(name);
+    return document.getElementById(name);
 }
 function error(message) {
-	EbyID("error-message").innerHTML = message;
+    EbyID("error-message").innerHTML = message;
 }
 //Note: progressbar is the INNER progressbar, NOT the outer.
 //progressbar should have already been EbyID'd.
 function updateProgressBar(progressbar, value, color) {
+    if(value < 0) {
+        value = 0;
+    }
     if(color == null) {
         healthRed = 127 - Math.floor(127 * (value / 100));
         healthGreen = Math.floor((196 * (value / 100)));
         color = "rgb(" + healthRed + ", " + healthGreen + ", 0)";
     }
-	progressbar.setAttribute("style", "width: " + (value * 2) + "px; background-color: " + color + ";");
+    progressbar.setAttribute("style", "width: " + (value * 2) + "px; background-color: " + color + ";");
 }
 function getPageName() {
-	return App.getStack()[App.getStack().length - 1][0];
+    return App.getStack()[App.getStack().length - 1][0];
 }
-function loadPage(name) {
-	App.load(name);
+function loadPage(name, transition) {
+    if(transition == null) {
+        App.load(name);
+    } else {
+        App.load(name, transition);
+    }
 }
